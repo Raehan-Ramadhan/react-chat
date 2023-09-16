ef0f4ad97e6bbc6e046439bace36e4b9d37ec899
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-17 00:06:43 +0700

    send message function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import 'firebase/compat/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
+import e from 'express';
 
 firebase.initializeApp({
   apiKey: "AIzaSyBE9YgYNtNXtA6uCu0r3E1uHSomOlnqVf4",
@@ -59,15 +61,34 @@ function ChatRoom() {
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+
+    const {uid, photoURL} = auth.currentUser;
+
+    await messagesref.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+  }
+
+
   return (
     <>
       <div>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
       </div>
 
-      <div>
-
-      </div>
+      <form onSubmit={sendMessage}>
+        <input value ={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+        <button type='submit'>Send</button>
+      </form>
     </>
   )
 }
@@ -75,7 +96,14 @@ function ChatRoom() {
 function ChatMessage(props) {
   const {text, uid} = props.message;
 
-  return <p>{text}</p>
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recieved';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL}/>
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
