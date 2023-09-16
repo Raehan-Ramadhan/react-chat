3407210038ea10239f23005a349de3b7d3041c9a
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-16 22:06:53 +0700

    sign in function

--- src/App.js
+++ src/App.js
@@ -32,4 +32,15 @@ function App() {
   );
 }
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  );
+}
+
 export default App;
